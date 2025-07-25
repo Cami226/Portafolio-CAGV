938f2c666f59ed1bf44dfb6f6d0c6d8855570bb1
[Camila] <cagv.cg@gmail.com>
2025-07-24 21:49:32 -0400

    Correcciones de formato

--- CAGV-P/assets/js/script.js
+++ CAGV-P/assets/js/script.js
@@ -1,11 +1,12 @@
-
-$(document).ready(function(){
-  $(".card").hover(function(){
+$(document).ready(function () {
+  $(".card").hover(function () {
     $(this).toggleClass("shadow-lg");
   });
 
-  $(".btn").click(function(e) {
+  $(".btn").click(function (e) {
     e.preventDefault();
-    alert("Iría al proyecto: " + $(this).closest('.card').find('.card-title').text());
+    alert(
+      "Iría al proyecto: " + $(this).closest(".card").find(".card-title").text()
+    );
   });
 });

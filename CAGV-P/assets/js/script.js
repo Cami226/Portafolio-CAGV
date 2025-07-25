9d83124967112dc51a60a06c4d3a9ea9ff743b7a
[Camila] <cagv.cg@gmail.com>
2025-07-24 21:47:40 -0400

    Ya termine todito

---
+++ CAGV-P/assets/js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $(".card").hover(function(){
+    $(this).toggleClass("shadow-lg");
+  });
+
+  $(".btn").click(function(e) {
+    e.preventDefault();
+    alert("Iría al proyecto: " + $(this).closest('.card').find('.card-title').text());
+  });
+});
